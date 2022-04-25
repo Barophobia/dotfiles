24052bf0c08a2709e4302a12ce9527fcddebd845
Barophobia <georgerosephone@gmail.com>
2022-04-25 08:09:25 +0000

    created install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Script to move the files from the github repo to the correct config location
+
+#Move ZSHRC
+cp -rfv ./.zshrc $HOME/
+
+#Move VIMRC
+cp -rfv ./.vimrc $HOME/
+
+#Move BASHRC
+cp -rfv ./.bashrc $HOME/
+
+#Move Config folder
+cp -rfv ./.config $HOME/
+
