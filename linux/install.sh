61fb894472037993af1d89bb0759f68a79cf3001
George <georgerosephone@gmail.com>
2022-06-02 18:55:51 +0100

    linux install script fixes

--- linux/install.sh
+++ linux/install.sh
@@ -1,14 +1,14 @@
 # Script to move the files from the github repo to the correct config location
 
 #Move ZSHRC
-cp -rfv ./.zshrc $HOME/
+cp -rfv $HOME/dotfiles/linux/.zshrc $HOME/
 
 #Move VIMRC
-cp -rfv ./.vimrc $HOME/
+cp -rfv $HOME/dotfiles/linux/.vimrc $HOME/
 
 #Move BASHRC
-cp -rfv ./.bashrc $HOME/
+cp -rfv $HOME/dotfiles/linux/.bashrc $HOME/
 
 #Move Config folder
-cp -rfv ./.config $HOME/
+cp -rfv $HOME/dotfiles/linux/.config $HOME/
 
